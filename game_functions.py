6a42975687ea9f6347a2940d4968cc259cecff28
steve-weng <steve_weng@hotmail.ca>
2019-10-08 12:07:07 -0400

    refactoring
    refactor keyup/keydown events

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,26 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = True
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = True
+	elif event.key == pygame.K_UP:
+		ship.moving_up = True
+	elif event.key == pygame.K_DOWN:
+		ship.moving_down = True
+
+def check_keyup_events(event, ship):
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = False
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = False
+	elif event.key == pygame.K_UP:
+		ship.moving_up = False
+	elif event.key == pygame.K_DOWN:
+		ship.moving_down = False
+
 # responds to keypresses and mouse events
 def check_events(ship):
 	
@@ -9,24 +29,10 @@ def check_events(ship):
 			sys.exit()
 
 		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_RIGHT:
-				ship.moving_right = True
-			elif event.key == pygame.K_LEFT:
-				ship.moving_left = True
-			elif event.key == pygame.K_UP:
-				ship.moving_up = True
-			elif event.key == pygame.K_DOWN:
-				ship.moving_down = True
+			check_keydown_events(event, ship)
 
 		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_RIGHT:
-				ship.moving_right = False
-			elif event.key == pygame.K_LEFT:
-				ship.moving_left = False
-			elif event.key == pygame.K_UP:
-				ship.moving_up = False
-			elif event.key == pygame.K_DOWN:
-				ship.moving_down = False
+			check_keyup_events(event, ship)
 
 # updates game screen
 def update_screen(ai_settings, screen, ship):
